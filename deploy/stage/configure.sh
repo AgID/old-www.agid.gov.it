0802d4d1d0fb7b3904d73f09952e411d8a1ef480
Davide Porrovecchio <pdavide@gmail.com>
2018-02-14 12:29:32 +0100

    Added stage environment recipe

---
+++ deploy/stage/configure.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+read -p "Provide username that hosts this project [agid]: " USER
+if [ -z "$USER" ]; then
+  USER=agid;
+fi
+
+read -p "Provide IP or FQDN of the target server [site.agid.local]: " HOST
+if [ -z "$HOST" ]; then
+  HOST=site.agid.local;
+fi
+
+read -p "Provide the absolute path to deploy to [/var/www/html]: " DEPLOY_TO
+if [ -z "$DEPLOY_TO" ]; then
+  DEPLOY_TO=/var/www/html;
+fi
+
+# Write config file for this project
+sed "s#@USER@#${USER}#g;s#@DEPLOY_TO@#${DEPLOY_TO}#g" deploy.yml.template > deploy.yml
+echo "Deploy file generated with provided parameters."
+
+sed "s#@DEPLOY_TO@#${DEPLOY_TO}#g" rollback.yml.template > rollback.yml
+echo "Rollback file generated with provided parameters."
+
+sed "s#@HOST@#${HOST}#g" hosts.template > hosts
+echo "Deploy host generated with provided parameters."
