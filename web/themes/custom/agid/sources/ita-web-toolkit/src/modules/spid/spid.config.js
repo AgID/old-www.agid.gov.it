8ea746f70ea417507ce96885624ca37b1d160f41
nino.marrazzo@wellnet.it <nino.marrazzo@wellnet.it>
2018-07-17 18:09:02 +0200

    AGID-363 FIX del widget di Share (EhRx6blD) - Aggiunge stile come da mockup.

---
+++ web/themes/custom/agid/sources/ita-web-toolkit/src/modules/spid/spid.config.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let IDP = [
+  { img: 'infocertid', idp: 'Infocert ID' },
+  { img: 'timid', idp: 'TIM ID' },
+  { img: 'posteid', idp: 'Poste ID' },
+  { img: 'sielteid', idp: 'Sielte ID' },
+  { img: 'arubaid', idp: 'Aruba ID' },
+]
+
+module.exports = { context: {IDP} }
