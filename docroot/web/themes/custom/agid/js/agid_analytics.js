ccd33ffe1e6592bb8374dbf517c8a2de3ad0d94d
Davide Porrovecchio <pdavide@gmail.com>
2022-05-13 16:05:22 +0200

    Disable hotjar and siteimprove

--- docroot/web/themes/custom/agid/js/agid_analytics.js
+++ docroot/web/themes/custom/agid/js/agid_analytics.js
@@ -25,26 +25,26 @@
       /**
        * Attach HotJar
        */
-      (function (h, o, t, j, a, r) {
-        h.hj = h.hj || function () {
-          (h.hj.q = h.hj.q || []).push(arguments)
-        };
-        h._hjSettings = {hjid: 338855, hjsv: 5};
-        a = o.getElementsByTagName('head')[0];
-        r = o.createElement('script');
-        r.async = 1;
-        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-        a.appendChild(r);
-      })(window, document, '//static.hotjar.com/c/hotjar-', '.js?sv=');
+      // (function (h, o, t, j, a, r) {
+      //   h.hj = h.hj || function () {
+      //     (h.hj.q = h.hj.q || []).push(arguments)
+      //   };
+      //   h._hjSettings = {hjid: 338855, hjsv: 5};
+      //   a = o.getElementsByTagName('head')[0];
+      //   r = o.createElement('script');
+      //   r.async = 1;
+      //   r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      //   a.appendChild(r);
+      // })(window, document, '//static.hotjar.com/c/hotjar-', '.js?sv=');
 
       /**
        * Attach siteimprove
        */
-      (function() {
-        var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
-        sz.src = '//siteimproveanalytics.com/js/siteanalyze_6086464.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
-      })();
+      // (function() {
+      //   var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
+      //   sz.src = '//siteimproveanalytics.com/js/siteanalyze_6086464.js';
+      //   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
+      // })();
     }
   }
 })(jQuery, Drupal);
